8178305a80f483222b415fffcf09d183986fb8a0
huangl42 <linwenh.huang@mail.utoronto.ca>
2018-11-18 13:12:04 -0500

    Made sure to reset array for all bullets back to default values when reset is pressed

--- bullet_maker.v
+++ bullet_maker.v
@@ -11,7 +11,7 @@ module bullet_maker(clk, pressed, reset, direct_x, direct_y, ship_x, ship_y);
     reg [6:0] start_y, curr_y [num_bullets - 1:0];
     reg [6:0] curr_y [num_bullets - 1:0];
 
-    integer i;
+    integer i, k;
     initial begin
         for(i = 0; i < num_bullets; i = i + 1)
             clk_array[i] <= clk;
@@ -51,6 +51,20 @@ module bullet_maker(clk, pressed, reset, direct_x, direct_y, ship_x, ship_y);
         begin: make_new_bullets
             if (reset) begin
                 count <= 8'b0;
+
+            for(k = 0; k < num_bullets; k = k + 1)
+    	        clk_array[i] <= clk;
+    	        shooting[i] <= 1'b0;
+    	        load[i] <= 1'b0;
+    	        reset_array[i] <= 1'b1;
+    	        collision[i] <= 1'b0;
+    	        start_x[i] <= 8'd0;
+    	        start_y[i] <= 7'd0;
+    	        direction_x[i] <= 2'b00;
+    	        direction_y[i] <= 2'b00;
+    	     end 
+
+		
             end
             else begin 
                 if(count == num_bullets - 1) begin
@@ -73,75 +87,4 @@ module bullet_maker(clk, pressed, reset, direct_x, direct_y, ship_x, ship_y);
 				end
             end
         end
-endmodule
-
-
-module bullet (clk, load, shooting, reset, firing, direction_x, direction_y, 
-		start_x, start_y, collision, curr_x, curr_y, plot_bullet);
-	input clk, reset, collision, load, shooting;
-	input [7:0] start_x;
-	input [6:0] start_y;
-	input [1:0] direction_x, direction_y;
-
-	output reg firing, plot_bullet;
-	reg [23:0] time_counter;
-	reg [1:0] curr_direct_x;
-	reg [1:0] curr_direct_y;
-	
-	output reg [7:0] curr_x;
-	output reg [6:0] curr_y;
-
-	always @ (posedge clk)
-		begin: draw_move
-			if (reset) begin
-				firing <= 1'b0; 		
-			end
-			else begin
-				if (collision) begin
-					firing <= 1'b0;
-					plot_bullet <= 1'b0;
-					//TODO: we need to figure out how to make the bullets disappear/end/terminate/kill itself/SIGINT/SIGKILL/-9 			
-
-				end
-				else if(shooting)
-					firing <= 1'b1;
-			end
-		end
-	//TODO make this work for multiple directions/rotation/8 directions 
-	always @(posedge clk)
-		begin: timer_count
-			if(load) begin
-				time_counter <= 24'd10;
-				curr_x <= start_x;
-				curr_y <= start_y;
-				plot_bullet <= 1'b0;
-				curr_direct_x <= direction_x;
-				curr_direct_y <= direction_y;					
-			end
-			else begin
-				time_counter <= time_counter == 24'd0 ? 24'd10 : time_counter - 1;
-				if(time_counter == 24'd0 && firing == 1'b1 && !collision) begin
-					plot_bullet <= 1'b1;
-					case (curr_direct_x)
-						2'b00: curr_x <= curr_x;
-						2'b01: curr_x <= curr_x + 1;
-						2'b10: curr_x <= curr_x - 1;
-						default: curr_x <= curr_x;
-					endcase
-				
-					case (curr_direct_y)
-						2'b00: curr_y <= curr_y;
-						2'b01: curr_y <= curr_y + 1;
-						2'b10: curr_y <= curr_y - 1;
-						default: curr_y <= curr_y;	
-					endcase
-				end
-				if (curr_x == 8'd159 || curr_y == 8'd119 || curr_x == 8'd0 || curr_y == 8'd0) begin
-					firing <= 1'b0;
-					plot_bullet = 1'b0;
-				end
-	
-			end
-		end
-	
 endmodule
